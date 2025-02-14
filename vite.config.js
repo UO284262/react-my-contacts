1ecbc463a079f6e0cc7f03738ef4016522ca2e14
Abel <UO284262@uniovi.es>
2025-02-14 19:48:37 +0100

    Solucionando mas cositas

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://UO284262.github.io/react-my-contacts"
+  base: "https://UO284262.github.io/react-my-contacts" 
 })
